0cf7c79fdd428b38328996614a375a8221e96faf
William Ruback <rubackw@gmail.com>
2022-02-11 13:24:12 -0500

    Added api route structure.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
+const { route } = require('../homeRoutes.js');
+const tagRoutes = require('./tagRoutes.js');
+const userbookRoutes = require('./userbookRoutes.js');
+const userRoutes = require('./userRoutes.js');
 
 
+router.use('/userbook', userbookRoutes);
+router.use('/tags', tagRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
