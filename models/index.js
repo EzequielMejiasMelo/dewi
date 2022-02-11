4b4f54280b2193635ad95e1909eb9432968930f2
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-02-11 12:57:08 -0500

    Wrote associations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
-
 const Author = require('./Author');
 const Books = require('./Books');
 const Tags = require('./Tags');
 
+Author.belongsToMany(Books, {through: 'AuthorBooks'});
+Books.belongsToMany(Author, {through: 'AuthorBooks'});
+
+Tags.belongsToMany(Books, {through: 'BookTags'});
+Books.belongsToMany(Tags, {through: 'BookTags'});
