22ce5f74dd7bafd5ad9efe1d6285d4c9b8c24402
William Ruback <rubackw@gmail.com>
2022-02-13 11:06:09 -0500

    Added tag selection functionality to the homepage.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,30 +1,59 @@
 const activeTags = $('#usedTags');
 const searchTag = $('#searchTag');
-const enterTag = $('#tagFinder');
 const popularTag = $('#popularTags');
+const searchButton = $('#startSearch');
 
-activeTags.on("click", function (event) {
+const taglist = ["Fiction"];
+
+activeTags.on("click","button", function (event) {
     console.log("Clicking!");
-    if (event.target.tagName.toLowerCase() === "button") {
-        $(event.target).parent().remove();
-    }
+    taglist.splice($(this).parent().index(), 1);
+    renderTagList();
 })
 
 popularTag.on("click", "button", function (event) {
     console.log("working");
-    activeTags.append(`<span class="tag is-danger m-1 is-large">
-    ${$(this).text()}
-    <button class="delete"></button>
-</span>`);
+    taglist.push($(this).text());
+    renderTagList();
 })
 
-enterTag.on("submit", function (event) {
-    event.preventDefault();
-    if (searchTag.val()) {
-        activeTags.append(`<span class="tag is-danger m-1 is-large">
-    ${searchTag.val()}
-    <button class="delete"></button>
-</span>`);
+searchButton.on('click', function(){
+    document.location.replace(`/search?tagsearch=${taglist.join(',')}`)
+});
+
+searchTag.autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: `/api/tags/autocomp/${request.term}`,
+            dataType: "json",
+            success: function (data) {
+                response($.map(data, function (item) {
+                    return {
+                        value: item.name
+                    };
+                }));
+            }
+        });
+    },
+    select: function (event, ui) {
+        event.preventDefault();
+        taglist.push(ui.item.value);
+        renderTagList();
         searchTag.val("");
     }
 });
+
+function renderTagList(){
+    activeTags.empty();
+    if(!taglist.length){
+        searchButton.prop("disabled",true);
+        return;
+    }
+    for(const tag of taglist){
+        activeTags.append(`<span class="tag is-danger m-1 is-large">
+    ${tag}
+    <button class="delete"></button>
+</span>`);
+    };
+    searchButton.prop("disabled",false);
+}
