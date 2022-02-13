f7daed928d6fe77b12fcbb525f929c27d1df6c30
William Ruback <rubackw@gmail.com>
2022-02-12 22:03:24 -0500

    Added search function to tag

--- controllers/api/tagRoutes.js
+++ controllers/api/tagRoutes.js
@@ -1,14 +1,39 @@
 const req = require('express/lib/request');
-
+const { Author, Books, Tags, AuthorBooks, TagBooks } = require('../../models');
 const router = require('express').Router();
 
 router.get("/", (req, res) => {
     res.json("Here will be the list of all tags.");
 });
 
-router.get("/searchbooks", (req, res) => {
+router.get("/searchbooks", async (req, res) => {
     // should have the queries limit and pagenum.
-    res.json(`Here will be the search results, but only ${req.query.limit} of them, and page ${req.query.pagenum}`);
+    try{
+    //console.log(req.body.tags);
+    const limit= req.query.limit ? parseInt(req.query.limit) : 10;
+    const pagenum= req.query.pagenum ? (limit*(parseInt(req.query.pagenum)-1))+1 : 1;
+    const tagsRaw = await Tags.findAll({where: {name: req.body.tags}});
+    const tags = tagsRaw.map(element => element.id);
+    let searchResults = await Books.findAll({
+        include: [{ model: Tags, through: {model: TagBooks, attributes:[]},where: {
+            id: tags
+        }, as: 'book_tags' }],
+        order: [['rating', 'DESC']],
+        attributes: ['id']
+    });
+
+    searchResults = searchResults.filter(element =>{return element.book_tags.length >= tags.length});
+    let output = [];
+    for(let i = pagenum-1; i < pagenum + limit-1 || i >= searchResults.length; i++){
+        output.push(await Books.findByPk(searchResults[i].id, {
+            include: [{ model: Tags, through: {model: TagBooks, attributes:[]}, as: 'book_tags'}]
+        }));
+    }
+    res.json(output);
+    }catch(err){
+        res.status(400).json('Cannot parse data.')
+        console.log(err);
+    }
 });
 
 router.get("/autocomp/:searchtext", (req, res) => {
