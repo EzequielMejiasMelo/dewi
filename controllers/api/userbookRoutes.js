05c65cf55dc54969cd0a7e83034f59c41eda4252
William Ruback <rubackw@gmail.com>
2022-02-14 17:14:48 -0500

    Fixed bugs with the favorite functions.

--- controllers/api/userbookRoutes.js
+++ controllers/api/userbookRoutes.js
@@ -5,32 +5,40 @@ const withAuth = require('../../utils/auth');
 
 const router = require('express').Router();
 
-router.put("/:id", withAuth, (req, res) => {
-    //This will update the book of id ${req.params.id} of whether it was read or not.
-});
-
-router.post("/:id", withAuth, async (req, res) => {
+router.post("/:id", async (req, res) => {
     //This will put the book of id ${req.params.id} into the user's favorites.
     try {
-        console.log('1');
+        if (!req.session.username) {
+            res.status(401).json("Not Logged in");
+            return;
+        }
         const book = await Books.findOne({
             where: {
                 id: req.params.id,
             }
         });
-        console.log('2');
+
         const user = await User.findOne({
             where: {
                 username: req.session.username,
             }
         });
-        console.log(user);
-        await UserBooks.create({
-            book_id: book.id,
-            user_id: user.id
+
+        const isFavorite = await UserBooks.findOne({
+            where: {
+                book_id: book.id,
+                user_id: user.id
+            }
         });
-        console.log('3');
-        res.status(200).json({addedBook: book, message: 'Book has been added.' });
+        if(!isFavorite){
+            await UserBooks.create({
+                book_id: book.id,
+                user_id: user.id
+            });
+            
+        }
+
+        res.status(200).json({ addedBook: book, message: 'Book has been added.' });
     } catch (err) {
         res.status(400).json(err);
     }
@@ -45,13 +53,19 @@ router.delete("/:id", withAuth, async (req, res) => {
             }
         });
 
-        await User.findOne({
+        const user = await User.findOne({
             where: {
                 username: req.session.username,
             }
-        }).removeBook(book);
+        });
 
-        res.status(200).json({removedBook: book, message: 'Book has been removed.'});
+        await UserBooks.destroy({
+            where: {
+                book_id: book.id,
+                user_id: user.id
+            }
+        });
+        res.status(200).json({ removedBook: book, message: 'Book has been removed.' });
     } catch (err) {
         res.status(400).json(err);
     }
